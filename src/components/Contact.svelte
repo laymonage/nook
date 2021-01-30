1976dd314a79ce21688a7da16af6f8d5a198a6bb
sage <laymonage@gmail.com>
2021-01-30 12:22:17 +0700

    :lipstick: Strip width classes from BaseCard

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -26,32 +26,34 @@
 	};
 </script>
 
-<BaseCard>
-	<slot slot="header">
-		<strong>{data.alias}</strong>
-	</slot>
+<div class="sm:w-10/12 lg:w-7/12 xl:w-6/12 mx-auto">
+	<BaseCard>
+		<slot slot="header">
+			<strong>{data.alias}</strong>
+		</slot>
 
-	<slot slot="subtitle">
-		<span class="text-gray-500 sm:ml-4 mr-1"> is </span>
-		<span>{data.name}</span>
-	</slot>
+		<slot slot="subtitle">
+			<span class="text-gray-500 sm:ml-4 mr-1"> is </span>
+			<span>{data.name}</span>
+		</slot>
 
-	<div class="text-left md:text-2xl text-xl ph:text-lg mb-8">
-		{data.description}
-	</div>
-	<div class="flex items-center">
-		{#each data.links as link}
-			<a
-				href={link.url}
-				class="ml-3 first:ml-0 p-1 rounded
-        focus:outline-none focus:bg-gray-200 focus:text-blue-600
-				hover:bg-gray-200 hover:text-blue-600 dark:text-blue-200
-				dark:focus:bg-blue-900 dark:focus:text-blue-100
-				dark:hover:bg-blue-900 dark:hover:text-blue-100
-				fill-current w-8 h-8"
-			>
-				<svelte:component this={link.icon} />
-			</a>
-		{/each}
-	</div>
-</BaseCard>
+		<div class="text-left md:text-2xl text-xl ph:text-lg mb-8">
+			{data.description}
+		</div>
+		<div class="flex items-center">
+			{#each data.links as link}
+				<a
+					href={link.url}
+					class="ml-3 first:ml-0 p-1 rounded
+					focus:outline-none focus:bg-gray-200 focus:text-blue-600
+					hover:bg-gray-200 hover:text-blue-600 dark:text-blue-200
+					dark:focus:bg-blue-900 dark:focus:text-blue-100
+					dark:hover:bg-blue-900 dark:hover:text-blue-100
+					fill-current w-8 h-8"
+				>
+					<svelte:component this={link.icon} />
+				</a>
+			{/each}
+		</div>
+	</BaseCard>
+</div>
