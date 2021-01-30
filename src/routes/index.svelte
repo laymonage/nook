dd6afc860eeab4f0077a1f8a64c99550b553c99b
sage <laymonage@gmail.com>
2021-01-30 07:31:24 +0700

    :lipstick: Use Contact component in index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,41 +1,15 @@
 <script>
-	import Counter from '$components/Counter.svelte';
+	import BaseContainer from '$components/base/BaseContainer.svelte';
+	import Contact from '$components/Contact.svelte';
 </script>
 
-<h1 class="bg-red-100 rounded-xl p-4">Hello world!</h1>
-
-<Counter />
-<p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+<BaseContainer>
+	<Contact />
+</BaseContainer>
 
 <style>
 	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-			'Open Sans', 'Helvetica Neue', sans-serif;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
+		font-family: -apple-system, BlinkMacSystemFont, Lato, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 	}
 </style>
