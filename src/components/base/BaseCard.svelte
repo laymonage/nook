d03ca58b16f0511115f6a69b0748692de695b056
sage <laymonage@gmail.com>
2021-01-30 07:29:57 +0700

    :sparkles: BaseCard and BaseContainer components

---
+++ src/components/base/BaseCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<div class="rounded bg-white dark:bg-gray-800 shadow-md flex-col
+  w-full xl:w-11/12 mx-auto p-8">
+	<div
+		class="text-blue-700 dark:text-blue-200 md:text-5xl text-4xl
+		mb-12 flex flex-col sm:flex-row items-baseline"
+	>
+		<slot name="header" />
+		<div v-if="$slots.subtitle" class="md:text-3xl text-2xl ph:text-xl">
+			<slot name="subtitle" />
+		</div>
+	</div>
+	<slot />
+</div>
