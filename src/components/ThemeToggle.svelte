48722afed9aa1e6f2313e02041bbed774f79be3f
sage <laymonage@gmail.com>
2021-01-31 23:37:32 +0700

    :sparkles: Create ThemeToggle component

---
+++ src/components/ThemeToggle.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { onMount } from 'svelte';
+	import Sun from '$components/icons/Sun.svelte';
+	import Moon from '$components/icons/Moon.svelte';
+
+	const themeIcons = {
+		light: Sun,
+		dark: Moon,
+	};
+	const themes = Object.keys(themeIcons);
+	let current = 'light';
+
+	async function toggle() {
+		const { classList } = document.querySelector('html');
+		const currentIndex = themes.findIndex((theme) => theme === current);
+		localStorage.theme = current = themes[currentIndex + 1] ?? themes[0];
+		classList.remove(...themes);
+		classList.add(current);
+	}
+
+	onMount(() => {
+		const prefersDark = window && matchMedia('(prefers-color-scheme: dark)');
+		const preferred = prefersDark && prefersDark.matches ? 'dark' : 'light';
+		current = (localStorage && localStorage.theme) ?? preferred;
+	});
+</script>
+
+<button
+	type="button"
+	class="focus:outline-none
+	focus:text-blue-600 hover:text-blue-600
+	dark:focus:text-blue-100 dark:hover:text-blue-100
+	fill-current w-6 h-6"
+	aria-label="Theme"
+	on:click={toggle}
+>
+	<svelte:component this={themeIcons[current]} />
+</button>
