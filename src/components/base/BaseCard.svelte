7cc714f72815d0f44d4df85562d4f987eb6da864
sage <laymonage@gmail.com>
2021-02-01 22:45:46 +0700

    :lipstick: Increase bottom margin on card heading"

--- src/components/base/BaseCard.svelte
+++ src/components/base/BaseCard.svelte
@@ -2,7 +2,7 @@
 	class="flex-col w-full p-8 transition-colors duration-500 bg-white rounded shadow-md dark:bg-gray-800 ease"
 >
 	<div
-		class="flex flex-col items-baseline mb-12 text-4xl text-blue-800 dark:text-blue-200 md:text-5xl sm:flex-row"
+		class="flex flex-col items-baseline mb-14 text-4xl text-blue-800 dark:text-blue-200 md:text-5xl sm:flex-row"
 	>
 		<slot name="header" />
 		{#if $$slots.subtitle}
