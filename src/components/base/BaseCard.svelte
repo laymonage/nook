91182153fe3a098cc268cc398a502f679f1129c2
sage <laymonage@gmail.com>
2021-02-07 11:50:48 +0700

    :lipstick: Make BaseCard header optional and adjust width

--- src/components/base/BaseCard.svelte
+++ src/components/base/BaseCard.svelte
@@ -1,15 +1,17 @@
 <div
-	class="flex-col w-full p-8 transition-colors duration-500 bg-white rounded shadow-md dark:bg-gray-800 ease"
+	class="flex-col w-full p-8 transition-colors duration-500 bg-white rounded shadow-md dark:bg-gray-800 ease min-w-min"
 >
-	<div
-		class="flex flex-col items-baseline mb-14 text-4xl text-blue-800 dark:text-blue-200 md:text-5xl sm:flex-row"
-	>
-		<slot name="header" />
-		{#if $$slots.subtitle}
-			<div class="text-2xl md:text-3xl">
-				<slot name="subtitle" />
-			</div>
-		{/if}
-	</div>
+	{#if $$slots.header}
+		<div
+			class="flex flex-col items-baseline mb-14 text-4xl text-blue-800 dark:text-blue-200 md:text-5xl sm:flex-row"
+		>
+			<slot name="header" />
+			{#if $$slots.subtitle}
+				<div class="text-2xl md:text-3xl">
+					<slot name="subtitle" />
+				</div>
+			{/if}
+		</div>
+	{/if}
 	<slot />
 </div>
