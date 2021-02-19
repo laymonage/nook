5215c314f719251c8217c045bd7a88a7f2eafaaa
sage <laymonage@gmail.com>
2021-02-19 14:34:44 +0700

    :rotating_light: Fix linter warnings

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,29 +1,29 @@
 // Consult https://www.snowpack.dev to learn about these options
 module.exports = {
-	extends: "@sveltejs/snowpack-config",
+	extends: '@sveltejs/snowpack-config',
 	plugins: [
 		[
-			"@snowpack/plugin-build-script",
+			'@snowpack/plugin-build-script',
 			{
-				cmd: "postcss",
-				input: [".css", ".pcss"],
-				output: [".css"],
+				cmd: 'postcss',
+				input: ['.css', '.pcss'],
+				output: ['.css'],
 			},
 		],
 		[
-			"@snowpack/plugin-svelte",
+			'@snowpack/plugin-svelte',
 			{
 				compilerOptions: {
 					hydratable: true,
 				},
 			},
 		],
-		"@snowpack/plugin-typescript",
+		'@snowpack/plugin-typescript',
 	],
 	mount: {
-		"src/components": "/_components",
+		'src/components': '/_components',
 	},
 	alias: {
-		$components: "./src/components",
+		$components: './src/components',
 	},
 };
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 const sveltePreprocess = require('svelte-preprocess');
 module.exports = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    preprocess: sveltePreprocess(),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: sveltePreprocess(),
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
@@ -10,6 +10,6 @@ module.exports = {
 		adapter: '@sveltejs/adapter-node',
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+	},
 };
